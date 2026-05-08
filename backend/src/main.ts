f8cbcfe2876423f519163fe61bd153f139eae6f0
sirfrajendra <sirfrajendra@gmail.com>
2026-05-08 10:37:43 +0545

    first commit

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3001);
+}
+bootstrap();
